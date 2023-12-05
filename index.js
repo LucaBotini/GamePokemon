ff51a4ca517303a17f468eef6480e403cffd2a7b
Botinixx <devbotini@gmail.com>
2023-12-05 17:33:14 -0300

    botão atk
    Adicionado efeito sonoro ao clicar em botão de ataque

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ let atkCharmander;
 let atkBulbasaur = gerarNumeroAleatorio(15, 25);
 let atkSquirtle = gerarNumeroAleatorio(8, 35);
 
+//CURA DE CADA POKÉMON
+let curaCharmander = gerarNumeroAleatorio(1, 17);
+let curaBulbasaur = gerarNumeroAleatorio(10, 14);
+let curaSquirtle = gerarNumeroAleatorio(4, 12);
+
 //MOSTAR VIDA DO RIVAL
 let mostrarhpInimigo = document.getElementById('hpInimigo')
 mostrarhpInimigo.textContent = `HP DO RIVAL: ${hpInimigo}`
 
 
-//MOSTAR VIDA DO MEU POKÉMON
+//MOSTRAR VIDA DO MEU POKÉMON
 let mostrarhpCharmanderr = document.getElementById('hpCharmanderr')
 mostrarhpCharmanderr.textContent = `HP DO CHARMANDER: ${hpCharmander}`
 
@@ -44,11 +49,20 @@ function executarAtaque() {
 }
 
 
-//CURA DE CADA POKÉMON
-let curaCharmander = gerarNumeroAleatorio(1, 17);
-let curaBulbasaur = gerarNumeroAleatorio(10, 14);
-let curaSquirtle = gerarNumeroAleatorio(4, 12);
+let barulhoatk = document.getElementById('botaoatk');
+let audio1 = new Audio('./musicas/efeitoatk.mp3');
+let audio2 = new Audio('./musicas/efeitoatk1.mp3');
+let alternarAudio = false;
 
+barulhoatk.addEventListener('click', () => {
+    if (alternarAudio) {
+      audio1.play();
+    } else {
+      audio2.play();
+    }
+  
+    alternarAudio = !alternarAudio;
+  });
 
 
 function gerarNumeroAleatorio(min, max) {
