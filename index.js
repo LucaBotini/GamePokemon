e6f6f681b9fc84a5c677810b46ed92fcd46e5263
Luca Botini <devbotini@gmail.com>
2023-12-05 21:31:02 -0300

    att v3.0.0
    Cura, Hit inimigo, Vitoria.
    Popup na tela explicando o botão.
    Funções JS.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let atkBulbasaur = gerarNumeroAleatorio(15, 25);
 let atkSquirtle = gerarNumeroAleatorio(8, 35);
 
 //CURA DE CADA POKÉMON
-let curaCharmander = gerarNumeroAleatorio(1, 17);
+let curaCharmander;
 let curaBulbasaur = gerarNumeroAleatorio(10, 14);
 let curaSquirtle = gerarNumeroAleatorio(4, 12);
 
@@ -25,33 +25,61 @@ mostrarhpInimigo.textContent = `HP DO RIVAL: ${hpInimigo}`
 let mostrarhpCharmanderr = document.getElementById('hpCharmanderr')
 mostrarhpCharmanderr.textContent = `HP DO CHARMANDER: ${hpCharmander}`
 
-
+//MENSAGEM NA TELA PÓS CLIQUES
+let popup = document.getElementById('popup')
 
 //FUNÇÃO AO CLICAR EM ATACAR
-   atkCharmander = gerarNumeroAleatorio(5, 40)
-
 function atkC(){
     let atkCharmander = gerarNumeroAleatorio(5, 40);
-    console.log(`ATK CHARMANDER ${atkCharmander}`);
+    popup.textContent = `VOCÊ ATACOU EQUIPE ROCKET COM ${atkCharmander} DE DANO`;
     return atkCharmander;
 }
 
 function executarAtaque() {
     let ataque = atkC();
     hpInimigo = hpInimigo - ataque;
-    console.log(`HP INIMIGO ${hpInimigo}`);
-
+    
     mostrarhpInimigo.textContent = `HP DO RIVAL: ${hpInimigo}`;
     if(hpInimigo <= 0){
         mostrarhpInimigo.textContent = `HP DO RIVAL: 0`;
+        esVitoria.play();
+        barulhoatk.disabled = true
         alert('Você Venceu!')
     }
 }
+
+//FUNÇÃO AO CLICAR EM CURAR
+function curarC(){
+  let curaCharmander = gerarNumeroAleatorio(1, 17);
+  popup.textContent = `VOCÊ SE CUROU COM ${curaCharmander} DE HP`;
+  return curaCharmander;
+}
+
+function executarCura() {
+  let curas = curarC();
+  hpCharmander = hpCharmander + curas;
+
+  mostrarhpCharmanderr.textContent = `HP DO CHARMANDER: ${hpCharmander}`;
+  if(hpCharmander <= 0){     
+
+
+      mostrarhpCharmanderr.textContent = `HP DO CHARMANDER: 0`;
+      alert('Você Perdeu!')
+  }
+}
+
+
+let barulhocura = document.getElementById('botaocura')
+let audioCura = new Audio('./musicas/esCura.mp3');
+barulhocura.addEventListener('click', () => {
+  audioCura.play();
+})
 
 
 let barulhoatk = document.getElementById('botaoatk');
 let audio1 = new Audio('./musicas/efeitoatk.mp3');
 let audio2 = new Audio('./musicas/efeitoatk1.mp3');
+let esVitoria = new Audio('./musicas/esVitoria.mp3');
 let alternarAudio = false;
 
 barulhoatk.addEventListener('click', () => {
@@ -71,3 +99,15 @@ function gerarNumeroAleatorio(min, max) {
 
 
 document.querySelector('audio').volume = 0.2;
+
+
+function exibirPopup() {
+  var popup = document.getElementById('popup');
+  
+  popup.style.display = 'block';
+
+  // Ocultar o popup após 3 segundos (3000 milissegundos)
+  setTimeout(function() {
+      popup.style.display = 'none';
+  }, 3000);
+}
